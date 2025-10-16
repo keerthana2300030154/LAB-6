4762638a5986bd4560c716ecd95ce963bdf57188
Keerthana <darapukeerthana2808@gmail.com>
2025-10-16 08:19:17 +0530

    updated

--- STOREAPI-BACKEND/src/main/java/com/klef/StoreapiBackendApplication.java
+++ STOREAPI-BACKEND/src/main/java/com/klef/StoreapiBackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StoreapiBackendApplication {
+public class StoreapiBackendApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoreapiBackendApplication.class, args);
